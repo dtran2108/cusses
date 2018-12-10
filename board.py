6764562c8ee32e5fd82983e4998510e71c10cbcc
dtran00 <dtran2108@gmail.com>
2018-12-10 22:47:16 +0700

    tetrixx

---
+++ board.py
@@ -0,0 +1,201 @@
+import math
+import random
+import os
+
+
+block_shapes = [
+    # T Block
+    [[0, 1, 0],
+     [1, 1, 1]],
+    # L Block
+    [[1, 0],
+     [1, 0],
+     [1, 1]],
+    # S Block
+    [[0, 1, 1],
+     [1, 1, 0]],
+    # O Block
+    [[1, 1],
+     [1, 1]],
+    # I Block
+    [[1], [1], [1], [1]],
+    # small O Block
+    [[1]]
+]
+
+
+class Board:
+    """Board representation"""
+
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+        self.board = self._get_new_board()
+
+        self.current_block_pos = None
+        self.current_block = None
+        self.next_block = None
+
+        self.game_over = False
+        self.score = None
+        self.level = None
+
+    def start(self):
+        """Start game"""
+
+        self.board = self._get_new_board()
+
+        self.current_block_pos = None
+        self.current_block = None
+        self.next_block = None
+
+        self.game_over = False
+        self.score = 0
+        self.level = 1
+
+        self._place_new_block()
+
+    def _get_new_board(self):
+        """Create new empty board"""
+
+        return [[0 for _ in range(self.width)] for _ in range(self.height)]
+
+    def _place_new_block(self):
+        """Place new block and generate the next one"""
+
+        if self.next_block is None:
+            self.current_block = self._get_new_block()
+            self.next_block = self._get_new_block()
+        else:
+            self.current_block = self.next_block
+            self.next_block = self._get_new_block()
+
+        size = Block.get_size(self.current_block.shape)
+        col_pos = math.floor((self.width - size[1]) / 2)
+        self.current_block_pos = [0, col_pos]
+
+        if self._check_overlapping(self.current_block_pos,
+                                   self.current_block.shape):
+            self.game_over = True
+
+    def _check_overlapping(self, pos, shape):
+        """If current block overlaps any other on the board"""
+        '''
+        size[0]: height of the block
+        size[1]: width of the block
+        '''
+        size = Block.get_size(shape)
+        for row in range(size[0]):
+            for col in range(size[1]):
+                if shape[row][col] == 1:
+                    if self.board[pos[0] + row][pos[1] + col] == 1:
+                        return True
+        return False
+
+    def _can_move(self, pos, shape):
+        """Check if move is possible"""
+        '''
+        pos[0]: y coordinate
+        pos[1]: x coordinate
+        2 steps:
+            1. Comparing the block with the board.
+            2. Comparing the block with the current landed blocks.
+        '''
+        size = Block.get_size(shape)
+        if pos[1] < 0 or pos[1] + size[1] > self.width \
+                or pos[0] + size[0] > self.height:
+            return False
+
+        return not self._check_overlapping(pos, shape)
+
+    def _burn(self):
+        """Remove matched lines"""
+
+        for row in range(self.height):
+            if all(col != 0 for col in self.board[row]):
+                for r in range(row, 0, -1):
+                    self.board[r] = self.board[r - 1]
+                self.board[0] = [0 for _ in range(self.width)]
+                self.score += 1
+                if self.score % 5 == 0:
+                    self.level += 1
+
+    def _land_block(self):
+        """Put block to the board and generate a new one"""
+
+        size = Block.get_size(self.current_block.shape)
+        for row in range(size[0]):
+            for col in range(size[1]):
+                if self.current_block.shape[row][col] == 1:
+                    current_block_x = self.current_block_pos[1]
+                    current_block_y = self.current_block_pos[0]
+                    self.board[current_block_y + row][
+                               current_block_x + col] = 1
+
+    def drop(self):
+        """Move to very very bottom"""
+
+        while self._can_move((self.current_block_pos[0] + 1,
+                             self.current_block_pos[1]),
+                             self.current_block.shape):
+            self.move_block("down")
+
+        self._land_block()
+        self._burn()
+        self._place_new_block()
+
+    def is_game_over(self):
+        """Is game over"""
+
+        return self.game_over
+
+    def rotate_block(self):
+        rotated_shape = list(map(list, zip(*self.current_block.shape[::-1])))
+
+        if self._can_move(self.current_block_pos, rotated_shape):
+            self.current_block.shape = rotated_shape
+
+    def move_block(self, direction):
+        """Try to move block"""
+
+        pos = self.current_block_pos
+        if direction == "left":
+            new_pos = [pos[0], pos[1] - 1]
+        elif direction == "right":
+            new_pos = [pos[0], pos[1] + 1]
+        elif direction == "down":
+            new_pos = [pos[0] + 1, pos[1]]
+
+        if self._can_move(new_pos, self.current_block.shape):
+            self.current_block_pos = new_pos
+        elif direction == "down":
+            self._land_block()
+            self._burn()
+            self._place_new_block()
+
+    @staticmethod
+    def _get_new_block():
+        """Get random block"""
+
+        block = Block(random.randint(0, len(block_shapes) - 1))
+
+        return block
+
+
+class Block:
+    """Block representation"""
+
+    def __init__(self, block_type):
+        self.shape = block_shapes[block_type]
+        self.color = block_type + 1
+
+    def size(self):
+        """Get size of the block"""
+
+        return self.get_size(self.shape)
+
+    @staticmethod
+    def get_size(shape):
+        """Get size of a shape"""
+
+        return [len(shape), len(shape[0])]
---
+++ tetris.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python3
+import curses
+import time
+import board
+
+
+# Initialize parameters
+BOARD_WIDTH = 10
+BOARD_HEIGHT = 17
+
+
+GAME_WINDOW_WIDTH = 2 * BOARD_WIDTH + 2
+GAME_WINDOW_HEIGHT = BOARD_HEIGHT + 2
+
+
+STATUS_WINDOW_HEIGHT = 12
+STATUS_WINDOW_WIDTH = 30
+
+BLOCK_SYMBOL = "  "
+
+
+def init_colors():
+    """Init colors"""
+
+    curses.init_pair(99, 8, curses.COLOR_BLACK)  # 1 - grey
+    curses.init_pair(98, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    curses.init_pair(97, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(96, curses.COLOR_BLACK, curses.COLOR_CYAN)
+    curses.init_pair(95, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_BLUE)
+    curses.init_pair(2, curses.COLOR_BLACK, 13)  # 13 - pink
+    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_YELLOW)
+    curses.init_pair(4, curses.COLOR_BLACK, curses.COLOR_GREEN)
+    curses.init_pair(5, curses.COLOR_BLACK, curses.COLOR_MAGENTA)
+    curses.init_pair(6, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
+
+def init_game_window():
+    """Create and return game window"""
+
+    window = curses.newwin(GAME_WINDOW_HEIGHT, GAME_WINDOW_WIDTH, 0, 0)
+    window.nodelay(True)
+    window.keypad(1)
+    return window
+
+
+def init_status_window():
+    """Create and return status window"""
+
+    window = curses.newwin(STATUS_WINDOW_HEIGHT, STATUS_WINDOW_WIDTH, 0,
+                           GAME_WINDOW_WIDTH)
+    return window
+
+
+def draw_game_window(window):
+    """Draw game window"""
+    '''
+    3 steps create the game window:
+        1. Drawing the board with its net and its finished blocks.
+        2. Find the position of the current dropping posistion and draw it.
+        3. Check the status of the game(Game Over or Pause) to draw
+        appropriate title
+    '''
+    window.border()
+
+    # draw board
+    '''
+    The board is a matrix which contains 0 and 1.
+    The position with value = 1 is the position is built IN FINISH by blocks.
+    '''
+    for a in range(BOARD_HEIGHT):
+        for b in range(BOARD_WIDTH):
+            if game_board.board[a][b] == 1:
+                window.addstr(a + 1, 2 * b + 1, BLOCK_SYMBOL,
+                              curses.color_pair(96))
+            else:
+                # draw net
+                window.addstr(a + 1, 2 * b + 1, " .", curses.color_pair(99))
+
+    # draw current block
+    for a in range(game_board.current_block.size()[0]):
+        for b in range(game_board.current_block.size()[1]):
+            if game_board.current_block.shape[a][b] == 1:
+                x = 2 * game_board.current_block_pos[1] + 2 * b + 1
+                y = game_board.current_block_pos[0] + a + 1
+                window.addstr(y, x, BLOCK_SYMBOL, curses.color_pair(
+                              game_board.current_block.color))
+
+    if game_board.is_game_over():
+        go_title = " Game Over "
+        ag_title = " Enter - play again "
+
+        window.addstr(int(GAME_WINDOW_HEIGHT*.4),
+                         (GAME_WINDOW_WIDTH-len(go_title))//2,
+                      go_title, curses.color_pair(95))
+        window.addstr(int(GAME_WINDOW_HEIGHT*.5),
+                         (GAME_WINDOW_WIDTH-len(ag_title))//2,
+                      ag_title, curses.color_pair(95))
+
+    if pause:
+        p_title = " Pause "
+        window.addstr(int(GAME_WINDOW_HEIGHT * .4),
+                         (GAME_WINDOW_WIDTH - len(p_title)) // 2,
+                      p_title, curses.color_pair(95))
+
+    window.refresh()
+
+
+def init_status_window():
+    """Create and return status window"""
+
+    window = curses.newwin(STATUS_WINDOW_HEIGHT, STATUS_WINDOW_WIDTH, 0,
+                           GAME_WINDOW_WIDTH)
+    return window
+
+
+def draw_status_window(window):
+    """Draw status window"""
+
+    if game_board.is_game_over():
+        return
+
+    # Initialize status window with empty space
+    for row in range(1, STATUS_WINDOW_HEIGHT - 1):
+        window.addstr(row, 2, (STATUS_WINDOW_WIDTH - 3)*" ")
+    window.border()
+    window.addstr(1, int(STATUS_WINDOW_WIDTH*.3),
+                  "Score: {}".format(game_board.score))
+    window.addstr(2, int(STATUS_WINDOW_WIDTH*.3),
+                  "Level: {}".format(game_board.level))
+    SCREEN_H, SCREEN_W = scr.getmaxyx()
+    if SCREEN_H <= GAME_WINDOW_HEIGHT + GAME_WINDOW_HEIGHT // 2 or \
+       SCREEN_W <= GAME_WINDOW_WIDTH * 2:
+        window.addstr(3, int(STATUS_WINDOW_WIDTH*.3), "WARNING!!!")
+        window.addstr(4, int(STATUS_WINDOW_WIDTH*.05),
+                      " YOUR GAME IS ABOUT TO STOP")
+
+    # Show the next board at the middle of the status win.
+    start_col = int(STATUS_WINDOW_WIDTH / 2 - game_board.next_block.size()[1])
+    # Color the next block
+    for row in range(game_board.next_block.size()[0]):
+        for col in range(game_board.next_block.size()[1]):
+            if game_board.next_block.shape[row][col] == 1:
+                window.addstr(6 + row, start_col + 2 * col, BLOCK_SYMBOL,
+                              curses.color_pair(game_board.next_block.color))
+
+    window.refresh()
+
+
+pause = False
+
+game_board = board.Board(BOARD_HEIGHT, BOARD_WIDTH)
+game_board.start()
+
+old_score = game_board.score
+
+if __name__ == "__main__":
+    try:
+        scr = curses.initscr()
+        curses.noecho()
+        curses.cbreak()
+        curses.start_color()
+        curses.curs_set(0)
+
+        init_colors()
+
+        game_window = init_game_window()
+        status_window = init_status_window()
+
+        draw_game_window(game_window)
+        draw_status_window(status_window)
+
+        start = time.time()
+
+        quit_game = False
+        while not quit_game:
+            key_event = game_window.getch()
+            draw_status_window(status_window)
+            # hack: redraw it on resize
+            if key_event == curses.KEY_RESIZE:
+                SCREEN_H, SCREEN_W = scr.getmaxyx()
+                if SCREEN_H <= GAME_WINDOW_HEIGHT \
+                        or SCREEN_W <= GAME_WINDOW_WIDTH:
+                    quit_game = True
+                game_window.clear()
+                draw_game_window(game_window)
+
+            if key_event == ord("q"):
+                quit_game = True
+
+            if not game_board.is_game_over():
+                if not pause:
+                    # Configuring delay speed
+                    if time.time() - start >= 1 / game_board.level:
+                        game_board.move_block("down")
+                        start = time.time()
+
+                    if key_event == curses.KEY_UP:
+                        game_board.rotate_block()
+                    elif key_event == curses.KEY_DOWN:
+                        game_board.move_block("down")
+                    elif key_event == curses.KEY_LEFT:
+                        game_board.move_block("left")
+                    elif key_event == curses.KEY_RIGHT:
+                        game_board.move_block("right")
+                    elif key_event == ord(" "):
+                        game_board.drop()
+                if key_event == ord("p"):
+                    pause = not pause
+                    game_window.nodelay(True)
+            else:
+                game_window.nodelay(False)
+                if key_event == ord("\n"):
+                    game_board.start()
+                    game_window.nodelay(True)
+
+            draw_game_window(game_window)
+
+            if old_score != game_board.score:
+                draw_status_window(status_window)
+                old_score = game_board.score
+    finally:
+        curses.endwin()
